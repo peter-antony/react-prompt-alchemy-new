2a5aff19ee706a49c993d576cec28c17af3c74fc
Antony.Peter <Antony.Peter@mycare.org.nz>
2025-11-17 20:32:09 +0530

    Removed footer in the latest added content due save button issues

--- src/stores/footerStore.ts
+++ src/stores/footerStore.ts
@@ -1,21 +1,17 @@
 import { create } from "zustand";
-export type FooterLabelConfig = {
-  label: string;
-  text: string;
-};
+
 export type FooterButtonConfig = {
   label: string;
-  onClick?: () => void;
+  onClick: () => void;
   disabled?: boolean;
   loading?: boolean;
-  type?: "Button" | "Icon" | 'Link' | 'Label';
+  type?: "Button" | "Icon" | 'Link';
   iconName?: string;
 };
 
 type FooterConfig = {
   visible: boolean;
   pageName: string;
-  leftLabel?: FooterLabelConfig; 
   leftButtons: FooterButtonConfig[];
   rightButtons: FooterButtonConfig[];
 };
